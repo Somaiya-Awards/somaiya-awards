f6d7e1d22e40dffed5a9596be1c83e28be4bc72c
Kiran D Jambhale <kiran.jambhale@SVV.local>
2023-11-01 16:37:11 +0530

    debug

--- backend/middleware/userAuthenticator.js
+++ backend/middleware/userAuthenticator.js
@@ -1,16 +1,20 @@
 const asyncHandler = require("express-async-handler");
 
 const userAuthenticator = asyncHandler( async (req,res,next)=>{
-    console.log('user_id '.req.headers['user_id']);
-    console.log('token '.req.headers['x-access-token']);
+
     const token = req.headers['x-access-token']
     const user_id = req.headers['user_id']
-
-    res.status(401).json({
-            message:' user_id'+user_id+'token '+token
+    
+    /**if something breaks remove this if statement due to token or userID while TESTING */
+    if(!token || !user_id){
+        res.status(401).json({
+            message:'Missing token and id'
         })
+    }
+    /**till here */
     
-    
+    res.token = token
+    res.user_id = user_id
     
     next()
 } )
