d7045bdcf2aa76fe7619df87f2cf2bf4b69c62c1
Kiran D Jambhale <kiran.jambhale@SVV.local>
2023-11-01 15:52:31 +0530

    debug

--- backend/middleware/userAuthenticator.js
+++ backend/middleware/userAuthenticator.js
@@ -5,6 +5,10 @@ const userAuthenticator = asyncHandler( async (req,res,next)=>{
     console.log('token '.req.headers['x-access-token']);
     const token = req.headers['x-access-token']
     const user_id = req.headers['user_id']
+
+    res.status(401).json({
+            message:' user_id'+user_id+'token '+token
+        })
     
     /**if something breaks remove this if statement due to token or userID while TESTING */
     if(!token || !user_id){
