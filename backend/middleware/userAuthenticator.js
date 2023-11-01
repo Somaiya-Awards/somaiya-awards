530c1c52b1a7c0d41a3ad980fecabd27ce4ceafb
Kiran D Jambhale <kiran.jambhale@SVV.local>
2023-11-01 22:58:29 +0530

    debug

--- backend/middleware/userAuthenticator.js
+++ backend/middleware/userAuthenticator.js
@@ -1,9 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const { authLogger } = require('logger');
 
 const userAuthenticator = asyncHandler( async (req,res,next)=>{
 
     const token = req.headers['x-access-token']
     const user_id = req.headers['user_id']
+    
+    authLogger.info(`user_id ${user_id}`)
+    authLogger.info(`token ${token}`)
     
     /**if something breaks remove this if statement due to token or userID while TESTING */
     if(!token || !user_id){
