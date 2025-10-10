b445268a27b3aaa91cd9df3831746e656c154ab4
Kiran <kiran.jambhale@somaiya.edu>
2025-10-10 19:28:29 +0530

    changes

--- backend/zod/auth/jwt.ts
+++ backend/zod/auth/jwt.ts
@@ -4,7 +4,7 @@ import { email, institute, role, validNumber, validString } from "..";
 export const JwtForm = z.object({
   id: validNumber,
   email_id: email,
-  institution: validString.optional().nullable(),
+  institution: validString.optional(),
   role: role.optional(),
 });
 
