7b226f465c5546b7808c790209ba3d631e81291e
Kiran <kiran.jambhale@somaiya.edu>
2025-11-12 14:34:23 +0530

    issue fixes

--- backend/zod/form/FeedbackFour.ts
+++ backend/zod/form/FeedbackFour.ts
@@ -6,27 +6,42 @@ import {
     serverTextArea,
     validString,
 } from "../../zod";
-import { agreeList, ratingList, feedTeacherCategory } from "../../constants";
+import { agreeList, PeerNonTeachingFeedbackList } from "../../constants";
 
 export const FeedbackFourForm = z.object({
     rater_name: validString,
-    institute_name: validString,
-    department_name: validString,
+
+    institution_name: validString,
+
+    department: validString,
+
     designation: validString,
+
     somaiya_mail_id: somaiyaMail,
-    contact_number: phoneNumber,
-    teacher_name: validString,
-    nomination_category: arrayChoice(feedTeacherCategory),
+
+    contact_no: phoneNumber,
+
+    nominee_name: validString,
+
+    category: arrayChoice(PeerNonTeachingFeedbackList),
+
     q_01: arrayChoice(agreeList),
+
     q_02: arrayChoice(agreeList),
-    q_03: arrayChoice(agreeList),
+
+    q_03: arrayChoice(["Outstanding", "Excellent", "Good", "Average", "Poor"]),
+
     q_04: arrayChoice(agreeList),
+
     q_05: arrayChoice(agreeList),
+
     q_06: arrayChoice(agreeList),
+
     q_07: arrayChoice(agreeList),
-    q_08: arrayChoice(ratingList),
-    q_09: arrayChoice(agreeList),
-    nomination_reason: serverTextArea({ maxLength: 600 }),
+
+    q_08: arrayChoice(agreeList),
+
+    nomination_reason: serverTextArea({ maxLength: 300 }),
 });
 
 export type FeedbackFourType = z.infer<typeof FeedbackFourForm>;
